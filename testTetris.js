14e613bea7d3ce54164f40db4e14e5ae15f6d93c
mathieu <mathieu.daguenet@outlook.com>
2024-05-13 23:46:54 +0200

    manque rotation add piece

--- testTetris.js
+++ testTetris.js
@@ -51,17 +51,86 @@ function drawPiece(canvas, piece){
         canvas.fillStyle = piece.color;
         //console.log(piece.color);
         //console.log(piece.positions[x].x , piece.positions[x].y );
-       // console.log(piece.positions[x].x * 40 , piece.positions[x].y * 40 );
+        // console.log(piece.positions[x].x * 40 , piece.positions[x].y * 40 );
         canvas.fillRect(piece.positions[x].x * 40, piece.positions[x].y * 40, 40, 40);//taille d'une case
     }
 }
 
-function fctRotate(direction, piece){
-   console.log(direction);
-   console.log(piece);
+// function fctRotate(piece){
+    //     console.log(piece);
+    //     for (let i = 0; i < piece.positions.length; i++)
+    //     {
+        //         tmpx = piece.positions[i].x;
+//         tmpy = piece.positions[i].y;
+//         tmpx -= piece.axe.x;
+//         tmpy -= piece.axe.y;
+//         piece.positions[i].x = tmpx * Math.cos(piece.angle * (Math.PI / 180)) - tmpy * Math.sin(piece.angle * (Math.PI / 180));
+//         piece.positions[i].y = tmpy * Math.sin(piece.angle * (Math.PI / 180)) + tmpx * Math.cos(piece.angle * (Math.PI / 180)); 
+//         piece.positions[i].x += piece.axe.x;
+//         piece.positions[i].y += piece.axe.y;
+//         Math.round(piece.positions[i].x);
+//         Math.round(piece.positions[i].y);
+//     }
+//     console.log(piece);
+//     console.log("angle ", piece.angle);
+// }
+
+// function fctRotate(piece) {
+//     console.log(piece);
+//     for (let i = 0; i < piece.positions.length; i++) {
+//         let tmpx = piece.positions[i].x;
+//         let tmpy = piece.positions[i].y;
+//         tmpx -= piece.axe.x;
+//         tmpy -= piece.axe.y;
+//         const newX = tmpx * Math.cos(piece.angle * (Math.PI / 180)) - tmpy * Math.sin(piece.angle * (Math.PI / 180));
+//         const newY = tmpy * Math.sin(piece.angle * (Math.PI / 180)) + tmpx * Math.cos(piece.angle * (Math.PI / 180)); 
+//         piece.positions[i].x = newX + piece.axe.x;
+//         piece.positions[i].y = newY + piece.axe.y;
+//         // piece.positions[i].x = Math.round(piece.positions[i].x);
+//         // piece.positions[i].y = Math.round(piece.positions[i].y);
+//     }
+//     console.log(piece);
+//     console.log("angle ", piece.angle);
+// }
+function isValid(piece, map){
+   // console.log("piece", piece);
+    for (let i = 0; i < piece.positions.length; i++)
+    {
+       // console.log(piece.positions[i].y, piece.positions[i].x);
+        // console.log(map[piece.positions[i].y][piece.positions[i].x]);
+        if (piece.positions[i].y > 19 || map[piece.positions[i].y][piece.positions[i].x] != "#FFFFFF")
+            return false;
+    }
+    return true;
 }
 
-function fctDown(speed, piece){
+function fctRotate(piece) {
+    //console.log("''''''''''''''''''''''''''''''''''''''''''", piece);
+//     for (let i = 0; i < piece.positions.length; i++) {
+//         let tmpx = piece.positions[i].y + (piece.axe.x - piece.axe.y);
+//         let tmpy = piece.positions[i].x + (piece.axe.y - piece.axe.x);
+//         piece.positions[i].x = tmpx;
+//         piece.positions[i].y = tmpy;
+//     }
+//     console.log(piece);
+    let tmp = [];
+    console.log("angle ", piece.angle);
+    for (let i = 0; i < piece.positions.length; i++) {
+        let tmpx, tmpy;
+        if (piece.angle === 90 || piece.angle === 180) { 
+            tmpx = piece.positions[i].y + (piece.axe.x - piece.axe.y);
+            tmpy = piece.positions[i].x + (piece.axe.y - piece.axe.x);
+        } else if (piece.angle === 270 || piece.angle === 0) { 
+            tmpx = piece.axe.x * 2 - piece.positions[i].x;
+            tmpy = piece.axe.y * 2 - piece.positions[i].y;
+        }
+        if (tmpx < 10 && tmpx > 0 && tmpy > 0 && tmpy < 20)
+            tmp.push({x:tmpx, y:tmpy});
+    }
+}
+
+
+function fctDown(piece){
    // console.log(speed);
    // console.log(piece);
     piece.axe.y += 1;
@@ -88,13 +157,64 @@ function initMap() {
 
 
 function getNextPiece(){
-    return ({
-            positions: [{x:4,y:0},{x:4,y:1},{x:4,y:2},{x:4,y:3}],
-            axe: {x: 4, y:1.5},
-            rotateFunction: fctRotate,
-            color:  "#FF0000",
-            movable: true
-        });
+    let piece = [{
+        positions: [{x:5,y:0},{x:4,y:0},{x:4,y:1},{x:4,y:2}],
+        axe: {x: 3.5, y:1.5},
+        angle: 0,
+        rotateFunction: fctRotate,
+        color:  "#7F00FF",
+        movable: true
+    },
+    {
+        positions: [{x:4,y:0},{x:3,y:0},{x:4,y:1},{x:4,y:2}],
+        axe: {x: 3.5, y:1.5},
+        angle: 0,
+        rotateFunction: fctRotate,
+        color:  "#00FF00",
+        movable: true
+    },
+    {
+        positions: [{x:4,y:0},{x:4,y:1},{x:4,y:2},{x:4,y:3}],
+        axe: {x: 3.5, y:1.5},
+        angle: 0,
+        rotateFunction: fctRotate,
+        color:  "#0000FF",
+        movable: true
+    },
+    {
+        positions: [{x:3,y:1},{x:4,y:1},{x:5,y:1},{x:4,y:0}],
+        axe: {x: 3.5, y:1.5},
+        angle: 0,
+        rotateFunction: fctRotate,
+        color:  "#FF0000",
+        movable: true
+    },
+    {
+        positions: [{x:3,y:0},{x:3,y:1},{x:4,y:0},{x:4,y:1}],
+        axe: {x: 3.5, y:1.5},
+        angle: 0,
+        rotateFunction: fctRotate,
+        color:  "#FFD700",
+        movable: true
+    },
+    {
+        positions: [{x:3,y:0},{x:4,y:0},{x:4,y:1},{x:5,y:1}],
+        axe: {x: 3.5, y:1.5},
+        angle: 0,
+        rotateFunction: fctRotate,
+        color:  "#808080",
+        movable: true
+    },
+    {
+        positions: [{x:4,y:0},{x:5,y:0},{x:3,y:1},{x:4,y:1}],
+        axe: {x: 3.5, y:1.5},
+        angle: 0,
+        rotateFunction: fctRotate,
+        color:  " #ED7F10",
+        movable: true
+    }];
+    console.log("rand = ", Math.round(Math.random()*10)%7);
+    return (piece[Math.round(Math.random()*10)%7]);
 }
 
 
@@ -104,17 +224,6 @@ function getNextPiece(){
 //     downFunction: fctDown
 // });
 
-function isValid(piece, map){
-   // console.log("piece", piece);
-    for (let i = 0; i < piece.positions.length; i++)
-    {
-       // console.log(piece.positions[i].y, piece.positions[i].x);
-        // console.log(map[piece.positions[i].y][piece.positions[i].x]);
-        if (piece.positions[i].y > 19 || map[piece.positions[i].y][piece.positions[i].x] != "#FFFFFF")
-            return false;
-    }
-    return true;
-}
 
 function addInMap(map, piece){
    // console.log("piece", piece);
@@ -137,103 +246,117 @@ function removeLine(map, line)
         }
         if (j == map[i].length)
         {
-            console.log("i ==", i);
+            //console.log("i ==", i);
             map.splice(i, 1);
             let newLine = new Array(10).fill("#FFFFFF");
             map.unshift(newLine);
             line+=1;
-            console.log("line", line);
+           // console.log("line", line);
         }
     }
 }
 
 let allowTimeout = true;
 
-const game = {
-    canevas: getCanevas(),
-    //downFunction: fctDown,
-    map: initMap(),
-    nextPiece: getNextPiece(),
-    currentPiece: getNextPiece(),
-    nbPiece:0,
-    line:0,
-    draw: function(){
-        drawCanevas(this.canevas, this.map);
-        drawPiece(this.canevas, this.currentPiece);
-    },
-    move: function(){
-        const self = this;
-        document.body.addEventListener("keydown", function(event, ){
-        allowTimeout = false;
-        console.log(event.key)
-        //console.log(self.currentPiece);
-        if (event.key == "a" || event.key == "d")
-        {
-            fctRotate(event.key, self.currentPiece);
-        }
-        //let pos = self.currentPiece.positions;
-        //for (let i = 0; i < pos.length; i++)
-        //  incrementer la vleur  a check
-        // if (event.key == "ArrowDown")
-        // {
-        //     for (let i = 0; i < self.currentPiece.positions.length; i++)
-        //     {
-        //         if (self.currentPiece.positions[i].y < 19)
-        //             self.currentPiece.positions[i].y += 1;
-        //     }
-        // }
-        if (event.key == "ArrowLeft")
-        {
-            for (let i = 0; i < self.currentPiece.positions.length; i++)
-            {
-                if (self.currentPiece.positions[i].x > 0)
-                    self.currentPiece.positions[i].x -= 1;
-            }
-        }
-        if (event.key == "ArrowRight")
-        {
-            for (let i = 0; i < self.currentPiece.positions.length; i++)
-            {
-                //console.log("in event", self.map[self.currentPiece.positions[i].y][self.currentPiece.positions[i].x+1]);
-                //console.log("in event", self.map[self.currentPiece.positions[i].y+1][self.currentPiece.positions[i].x]);
-                if (self.currentPiece.movable && self.currentPiece.positions[i].x < 9 /* || self.map[self.currentPiece.positions[i].y + 1][self.currentPiece.positions[i].x] == "#FFFFFF"*/)
-                    self.currentPiece.positions[i].x += 1;
-            }
-        }
-        allowTimeout = true;
-    })},
-    fall: function (speed){
-        const intervalId = setInterval(() => {
-            console.log("line", this.line);
-            while (allowTimeout == false)
-                ;
-            fctDown(speed, this.currentPiece);
-            if (isValid(this.currentPiece, this.map))
-                this.draw();
-            else
-            {
-                //this.currentPiece.movable = false
-                addInMap(this.map, this.currentPiece)
-                removeLine(this.map, this.line);
-                console.log("line ",this.line);
-                this.currentPiece = this.nextPiece;
-                this.nextPiece = getNextPiece();
-                this.nbPiece +=1;
-            }
-        }, 300);
-    },
-    start: function(){
-       // console.log("coucou");
-        //console.log(this.canevas);
-        this.draw()
-        this.move();
-        this.fall();
-    }
-};
+// const game = {
+//     canevas: getCanevas(),
+//     //downFunction: fctDown,
+//     map: initMap(),
+//     nextPiece: getNextPiece(),
+//     currentPiece: getNextPiece(),
+//     moved:0,
+//     nbPiece:0,
+//     line:0,
+//     draw: function(){
+//         drawCanevas(this.canevas, this.map);
+//         drawPiece(this.canevas, this.currentPiece);
+//     },
+//     move: function(self){
+//         document.body.addEventListener("keydown", function(event, ){
+//         allowTimeout = false;
+//        // console.log(event.key)
+//        // console.log(self.currentPiece);
+//         if (event.key == "a" || event.key == "d")
+//         {
+//             self.moved = 1;
+//             if (event.key == "a")
+//                 self.currentPiece.angle -= 90;
+//             else
+//                 self.currentPiece.angle += 90;
+//            // console.log("angle", self.currentPiece.angle);
+//             if (self.currentPiece.angle > 359)
+//                 self.currentPiece.angle -= 360;
+//             else if (self.currentPiece.angle < 0)
+//                 self.currentPiece.angle += 360;
+//             //console.log("angle", self.currentPiece.angle);
+//         }
+//         if (event.key == "ArrowLeft")
+//         {
+//             for (let i = 0; i < self.currentPiece.positions.length; i++)
+//             {
+//                 if (self.currentPiece.positions[i].x > 0)
+//                     self.currentPiece.positions[i].x -= 1;
+//             }
+//         }
+//         if (event.key == "ArrowRight")
+//         {
+//             for (let i = 0; i < self.currentPiece.positions.length; i++)
+//             {
+//                 //console.log("in event", self.map[self.currentPiece.positions[i].y][self.currentPiece.positions[i].x+1]);
+//                 //console.log("in event", self.map[self.currentPiece.positions[i].y+1][self.currentPiece.positions[i].x]);
+//                 if (self.currentPiece.movable && self.currentPiece.positions[i].x < 9 /* || self.map[self.currentPiece.positions[i].y + 1][self.currentPiece.positions[i].x] == "#FFFFFF"*/)
+//                     self.currentPiece.positions[i].x += 1;
+//             }
+//         }
+//        // console.log("angle fin event", self.currentPiece.angle);
+//         allowTimeout = true;
+//     })},
+//     fall: function (speed){
+//         setInterval(() => {
+//            // console.log("line", this.line);
+//             while (allowTimeout == false)
+//                 ;
+//             fctDown(speed, this.currentPiece);
+//             //console.log("piece", this.currentPiece);
+//             if (this.moved)
+//             {
+//                 console.log("//////////////////////////////////////////////////////////////////");
+//                 fctRotate(this.currentPiece);
+//                 this.moved = 0;
+//             }
+//             if (isValid(this.currentPiece, this.map))
+//                 this.draw();
+//             else
+//             {
+//                 addInMap(this.map, this.currentPiece)
+//                 removeLine(this.map, this.line);
+//                // console.log("line ",this.line);
+//                 this.currentPiece = this.nextPiece;
+//                 this.id++;
+//                 this.nextPiece = getNextPiece(this.id);
+//                 this.nbPiece +=1;
+//             }
+//             console.log("angle", this.currentPiece.angle);
+//            // console.log("game", this);
+//         },300);
+//     },
+//     start: function(){
+//         this.draw()
+//         this.fall();
+//     }
+// };
 
-game.start();
+// game.move(game);
 
-// piece.startFalling(1); // dans l'objet game
+
+// // function display()
+// // {
+// //     game.draw();
+// //     requestAnimationFrame(display);
+// // }
+// // display();
+// game.start();
+
 
 
 
@@ -250,3 +373,145 @@ game.start();
 // [{x:3,y:1},{x:4,y:1},{x:5,y:1},{x:6,y:1}]
 // [{x:5,y:0},{x:5,y:1},{x:5,y:2},{x:5,y:3}]
 
+// floodfill(char **tab, startx, starty)
+// (
+//     floodfill()
+//     floodfill()
+//     floodfill()
+//     floodfill()
+// )
+
+// 000
+// 999
+
+
+// sans timeinterval
+const game = {
+    canevas: getCanevas(),
+    map: initMap(),
+    nextPiece: getNextPiece(),
+    currentPiece: getNextPiece(),
+    moved:0,
+    nbPiece:0,
+    line:0,
+    time: performance.now(),
+    draw: function(){
+        drawCanevas(this.canevas, this.map);
+        drawPiece(this.canevas, this.currentPiece);
+    },
+    move: function(self){
+        document.body.addEventListener("keydown", function(event, ){
+        allowTimeout = false;
+        if (event.key == "a" || event.key == "d")
+        {
+            self.moved = 1;
+            if (event.key == "a")
+                self.currentPiece.angle -= 90;
+            else
+                self.currentPiece.angle += 90;
+           // console.log("angle", self.currentPiece.angle);
+            if (self.currentPiece.angle > 359)
+                self.currentPiece.angle -= 360;
+            else if (self.currentPiece.angle < 0)
+                self.currentPiece.angle += 360;
+            //console.log("angle", self.currentPiece.angle);
+        }
+        let newpos=[];
+        console.log("newpos 1 ", newpos);
+        console.log(self.currentPiece);
+        console.log("map ", self.map);
+        if (event.key == "ArrowLeft")
+        {
+            for (let i = 0; i < self.currentPiece.positions.length; i++)
+            {
+                //console.log("in event", self.map[self.currentPiece.positions[i].y][self.currentPiece.positions[i].x+1]);
+                //console.log("in event", self.map[self.currentPiece.positions[i].y+1][self.currentPiece.positions[i].x]);
+                if (self.currentPiece.positions[i].x - 1 >= 0 && self.map[self.currentPiece.positions[i].y][self.currentPiece.positions[i].x-1] == "#FFFFFF" /* || self.map[self.currentPiece.positions[i].y + 1][self.currentPiece.positions[i].x] == "#FFFFFF"*/)
+                    newpos.push({x:self.currentPiece.positions[i].x - 1, y:self.currentPiece.positions[i].y});
+            }
+            console.log(newpos);
+            if (newpos.length == 4)
+                self.currentPiece.positions = newpos;
+        }
+        if (event.key == "ArrowRight")
+        {
+            for (let i = 0; i < self.currentPiece.positions.length; i++)
+            {
+               // console.log("in event", self.map[self.currentPiece.positions[i].y][self.currentPiece.positions[i].x+1]);
+               console.log("x + 1 ", self.currentPiece.positions[i].x + 1 );
+               console.log("y ", self.currentPiece.positions[i].y );
+                console.log("in event", self.map[self.currentPiece.positions[i].y][self.currentPiece.positions[i].x+1]);
+                if (self.currentPiece.positions[i].x + 1 < 10 && self.map[self.currentPiece.positions[i].y][self.currentPiece.positions[i].x+1] == "#FFFFFF" /* || self.map[self.currentPiece.positions[i].y + 1][self.currentPiece.positions[i].x] == "#FFFFFF"*/)
+                    newpos.push({x:self.currentPiece.positions[i].x + 1, y:self.currentPiece.positions[i].y});
+            }
+            console.log("newpos 2 ", newpos);
+            if (newpos.length == 4)
+                self.currentPiece.positions = newpos;
+        }
+       // console.log("angle fin event", self.currentPiece.angle);
+        allowTimeout = true;
+    })},
+    fall: function (speed){
+        // console.log("perf now ", performance.now());
+        // console.log("game time ", game.time);
+        // let end = performance.now();
+        // console.log("dif", (end - game.time));
+        // if ((end - game.time) > 17)
+        // {
+        //     while (allowTimeout == false)
+        //         ;
+        //     fctDown(speed, game.currentPiece);
+        //     if (game.moved)
+        //     {
+        //         fctRotate(game.currentPiece);
+        //         game.moved = 0;
+        //     }
+        //     if (isValid(game.currentPiece, game.map))
+        //         game.draw();
+        //     else
+        //     {
+        //         addInMap(game.map, game.currentPiece)
+        //         removeLine(game.map, game.line);
+        //         game.currentPiece = game.nextPiece;
+        //         game.id++;
+        //         game.nextPiece = getNextPiece(game.id);
+        //         game.nbPiece += 1;
+        //     }
+        // }
+            //console.log(game.nbPiece);
+        game.draw();
+        game.time = performance.now();
+        requestAnimationFrame(game.fall);
+        },
+    start: function(){
+        this.fall();
+        this.move(this);
+    }
+};
+
+function fallPiece(game)
+{
+    setInterval(() => {
+        while (allowTimeout == false)
+        ;
+    fctDown(game.currentPiece);
+    if (game.moved)
+    {
+        fctRotate(game.currentPiece);
+        game.moved = 0;
+    }
+    if (isValid(game.currentPiece, game.map))
+    game.draw();
+    else
+    {
+        addInMap(game.map, game.currentPiece)
+        removeLine(game.map, game.line);
+        game.currentPiece = game.nextPiece;
+        game.id++;
+        game.nextPiece = getNextPiece(game.id);
+        game.nbPiece += 1;
+    }}, 200);
+}
+fallPiece(game);
+game.time = performance.now();
+game.start();
